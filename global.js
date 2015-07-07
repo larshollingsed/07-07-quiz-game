d13013e43526aea9bd9f7e9d7ff1e19b11679a95
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-07 11:30:10 -0500

    Moved questions into array of JS objects and looped through

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
-var correct = 0
-var wrong = 0
+var correct = 0;
+var wrong = 0;
 function winOrLose (answer, guess) {
     if (answer == guess.toLowerCase()) {
       correct++
@@ -8,39 +8,25 @@ function winOrLose (answer, guess) {
       wrong++
       alert("Incorrect :( ")
     }
+};
+
+var question1 = {question: "This ballet, composed by Igor Stravinsky and choreographed by Vaslav Nijinsky, almost caused a riot at its 1913 premiere.\nA - Coppelia\nB - The Rite of Spring\nC - Les Sylphides\nD - The Nutcracker", answer: "b"};
+
+var question2 = {question: "Twyla Tharp won a Tony for Best Choroeography in 2003 for this Broadway show, based on the music of Billy Joel.\nA - Kinky Boots\nB - Spiderman: Turn Off the Dark\nC - Movin' Out\nD - Hairspray", answer: "c"};
+
+var question3 = {question: "An average cat has this many tails.\nA - 1\nB - 2\nC - 3\nD - 4", answer: "a" };
+
+var question4 = {question: "This is not gluten free.\nA - a Nectarine\nB - a Brussel Sprout\nC - a Chicken Egg\nD - a Beer", answer: "d" };
+
+var questions = [question1, question2, question3, question4];
+
+for (index = 0, guess = "z"; index < questions.length; index++, guess = "z") {
+    while (guess !== "a" && guess !== "b" && guess !== "c" && guess !== "d") {
+      var guess = prompt(questions[index].question, "Enter A, B, C, or D")
+      guess = guess.toLowerCase()
+    }
+    winOrLose(questions[index].answer, guess)
 }
-
-var answer = "b"
-var guess = "z"
-while (guess.toLowerCase() !== "a" && guess.toLowerCase() !== "b" && guess.toLowerCase() !== "c" && guess.toLowerCase() !== "d") {
-    var guess = prompt("This ballet, composed by Igor Stravinsky and choreographed by Vaslav Nijinsky, almost caused a riot at its 1913 premiere.\nA - Coppelia\nB - The Rite of Spring\nC - Les Sylphides\nD - The Nutcracker", "Enter A, B, C, or D")
-}
-
-winOrLose (answer, guess)
-
-var answer = "c"
-var guess = "z"
-while (guess.toLowerCase() !== "a" && guess.toLowerCase() !== "b" && guess.toLowerCase() !== "c" && guess.toLowerCase() !== "d") {
-    var guess = prompt("Twyla Tharp won a Tony for Best Choroeography in 2003 for this Broadway show, based on the music of Billy Joel.\nA - Kinky Boots\nB - Spiderman: Out of Darkness\nC - Movin' Out\nD - Hairspray", "Enter A, B, C, or D")
-}
-
-winOrLose(answer, guess)
-
-var answer = "a"
-var guess = "z"
-while (guess.toLowerCase() !== "a" && guess.toLowerCase() !== "b" && guess.toLowerCase() !== "c" && guess.toLowerCase() !== "d") {
-    var guess = prompt("An average cat has this many tails.\nA - 1\nB - 2\nC - 3\nD - 4", "Enter A, B, C, or D")
-}
-
-winOrLose(answer, guess)
-
-var answer = "d"
-var guess = "z"
-while (guess.toLowerCase() !== "a" && guess.toLowerCase() !== "b" && guess.toLowerCase() !== "c" && guess.toLowerCase() !== "d") {
-    var guess = prompt("This is not gluten free.\nA - a Nectarine\nB - a Brussel Sprout\nC - a Chicken Egg\nD - a Beer", "Enter A, B, C, or D")
-}
-
-winOrLose(answer, guess)
 
 var rounds = wrong + correct
-alert ("Out of " + rounds + " questions, you answered " + correct + " correctly\n" + (correct / rounds * 100).toFixed(0) + "% correct")
+alert ("Out of " + rounds + " questions, you answered " + correct + " correctly\n" + (correct / rounds * 100).toFixed(0) + "% correct");
